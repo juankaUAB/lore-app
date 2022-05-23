b938d8aa6c8ce353b5ff1f23ec51d2dd8c486d45
jdouton <josep.douton.martin@gmail.com>
2022-05-23 11:54:04 +0200

    Sign in con problemas

--- app/src/main/java/com/example/lore1/MainActivity.kt
+++ app/src/main/java/com/example/lore1/MainActivity.kt
@@ -1,15 +1,45 @@
 package com.example.lore1
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-
+import com.example.lore1.databinding.ActivityMainBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var auth: FirebaseAuth
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        //setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        auth = Firebase.auth
+
+        // Configure Google Sign In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("825172834751-pb6o2qfb3og005brkjuin4mkt6qmoa9k.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
 
         val boton_tryit = findViewById<Button>(R.id.try_it)
         boton_tryit.setOnClickListener {
@@ -17,16 +47,61 @@ class MainActivity : AppCompatActivity() {
             startActivity(lanzar)
         }
 
-        val boton_tryit_log = findViewById<Button>(R.id.login)
-        boton_tryit_log.setOnClickListener {
-            val lanzar = Intent(this, Tryit_log::class.java)
-            startActivity(lanzar)
+        binding.signin.setOnClickListener {
+            signIn()
         }
+    }
 
-        val boton_signin = findViewById<Button>(R.id.signin)
-        boton_signin.setOnClickListener {
-            val lanzar = Intent(this, SignIn::class.java)
-            startActivity(lanzar)
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)!!
+                Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e)
+            }
         }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    updateUI(null)
+                }
+            }
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        if (user != null) {
+            val intent = Intent(applicationContext, Tryit_log::class.java)
+            intent.putExtra(EXTRA_NAME, user.displayName)
+            startActivity(intent)
+        }
+    }
+
+    companion object {
+        const val RC_SIGN_IN = 1001
+        const val EXTRA_NAME = "EXTRA_NAME"
     }
 }
--- app/src/main/java/com/example/lore1/Tryit_log.kt
+++ app/src/main/java/com/example/lore1/Tryit_log.kt
@@ -4,11 +4,25 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import com.example.lore1.MainActivity.Companion.EXTRA_NAME
+import com.example.lore1.databinding.ActivityTryitLogBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class Tryit_log : AppCompatActivity() {
+
+    private lateinit var binding: ActivityTryitLogBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tryit_log)
+        //setContentView(R.layout.activity_tryit_log)
+        binding = ActivityTryitLogBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var nombreUser = intent.getStringExtra(EXTRA_NAME)
+
+        binding.username.setText(nombreUser)
 
         val boton_tryit_texto = findViewById<Button>(R.id.tryit_log_texto)
         boton_tryit_texto.setOnClickListener {
@@ -34,8 +48,11 @@ class Tryit_log : AppCompatActivity() {
             startActivity(lanzar)
         }
 
-        val boton_signout = findViewById<Button>(R.id.tryit_log_signout)
-        boton_signout.setOnClickListener {
+        //val boton_signout = findViewById<Button>(R.id.tryit_log_signout)
+        binding.tryitLogSignout.setOnClickListener {
+
+            Firebase.auth.signOut();
+
             val lanzar = Intent(this, MainActivity::class.java)
             startActivity(lanzar)
         }
